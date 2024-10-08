74c9cf4f926f12cdb841a3b595f5637035986b86
Lookandme <kgg9386@gmail.com>
2024-10-08 20:42:33 +0900

    fix : 카메라 무빙 구현

---
+++ Assets/Scripts/CameraObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CameraObject : MonoBehaviour
+{
+   [SerializeField] private GameObject Taget;
+    private float followSpeed;
+    private float cameraRotationZ;
+
+    private Transform tagetTransform;
+    private Transform thisTrnansform;
+
+    private void Awake()
+    {
+       tagetTransform = Taget.GetComponent<Transform>();
+        thisTrnansform = GetComponent<Transform>();
+
+        followSpeed = 4.0f;
+        cameraRotationZ = -10.0f;
+    }
+
+
+
+    private void Update()
+    {
+        thisTrnansform.position =  Vector2.Lerp(thisTrnansform.position, tagetTransform.position, followSpeed * Time.deltaTime);
+        thisTrnansform.Translate(0,0, cameraRotationZ);
+    }
+    
+
+}
+
